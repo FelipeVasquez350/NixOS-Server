ef49df3b27cf66ea260acb366acec87cdf96683a
FelipeVasquez350 <Felipe350@proton.me>
2025-03-05 10:48:59 +0100

    Added dops

--- configuration.nix
+++ configuration.nix
@@ -136,6 +136,29 @@
   };
 
   systemd.services = {
+    dops-setup = {
+      description = "Setup Better Docker PS";
+      requires = [ "docker.service" ];
+      after = [ "docker.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+
+      # Specify the path to the packages needed for the script
+      path = [ pkgs.curl ];
+      environment = {
+        "PATH" = lib.mkForce "${pkgs.curl}/bin:${pkgs.coreutils}/bin:/run/current-system/sw/bin";
+      };
+
+      script = ''
+        # Install Better Docker PS
+        curl -L "https://github.com/Mikescher/better-docker-ps/releases/latest/download/dops_linux-amd64-static" -o "/usr/bin/dops" && chmod +x "/usr/bin/dops"
+      '';
+    };
+
     docker-swarm-init = {
       description = "Docker Swarm Init";
       requires = [ "docker.service" ];
@@ -240,6 +263,13 @@
         fi
       '';
     };
+  };
+
+  environment.variables = {
+    PATH = [
+      "/usr/bin"
+      "$PATH"
+    ];
   };
 
   # Enable automatic login for the user.
